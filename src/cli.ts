dfefb39092492fa7f11a1d6a256fde59cd4a51ff
StianM <stian.myklebostad@schibsted.com>
2020-08-28 14:34:47 +0200

    💡 Create cli main function

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,51 @@
+import NGMApi from "./api"
+import commands, { CommandFn } from './cli-commands'
+import ExecSequence, { SequenceFunc } from './utils/exec-sequence'
+import init_flags from "./cli-flags"
 
-const commands = new Map()
+const simple_usage = `usage: ngm <COMMAND> [OPTIONS]
+  use the -h option for more info
+`
 
+export default async (): Promise<void> => {
 
+  NGMApi.Init(process.cwd())
+  const cmds = commands()
+  const ngm_flags = init_flags()
+
+  const params: string[] = []
+  let command = ''
+  const ngm_handlers: SequenceFunc[] = []
+
+  process.argv.forEach(arg => {
+    if (cmds.has(arg))
+      command = arg
+    else if (ngm_flags.has(arg))
+      ngm_handlers.push(ngm_flags.get(arg) as SequenceFunc)
+    else
+     params.push(arg)
+  })
+
+  const runner = new ExecSequence()
+
+  ngm_handlers.forEach(handler => runner.push(handler))
+
+  runner
+    .push(async (next, err): Promise<void> => {
+      try {
+        await (cmds.get(command) as CommandFn)(NGMApi.Instance, ...params)
+        next()
+      } catch (e) {
+        err(`Status command failed\n\t${e.message || 'unknown error'}`)
+      }
+    })
+
+  try {
+    await runner.exec()
+  } catch (e) {
+    console.error(`[ERROR]  ${e.message}\n\n${simple_usage}`)
+  }
+
+  return
+  
+}
