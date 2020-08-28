7bea70aca0ce6415196b6a2ee700862f1896453c
StianM <stian.myklebostad@schibsted.com>
2020-08-28 09:33:09 +0200

    🔬 Husky testing

--- src/subroutines/index-fs.ts
+++ src/subroutines/index-fs.ts
@@ -16,6 +16,8 @@ const index_fs = (dir: string, ...sub_dir: string[]): Promise<string[]> => {
 
       folders = folders.filter(v => !v.startsWith('.'))
 
+      asd
+
       const sub_repos = (await Promise.all(
         folders.map((folder) => index_fs(path, folder))
       )).reduce((acc, folders) => ([...acc, ...folders]), [])
