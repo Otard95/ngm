9c128c6372c6ae01628b3651b9fd41adb0bef824
StianM <stian.myklebostad@schibsted.com>
2020-09-25 13:54:43 +0200

    project command finalized

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,9 @@
-import { NGMDot, ProjectId } from "./interfaces/ngm-dot"
+import { ModuleId, NGMDot, Project, ProjectId } from "./interfaces/ngm-dot"
 import read_dot from "./subroutines/read-dot"
 import { status } from "./git-commands"
 import { ModuleWithStatus } from "./interfaces/status"
+import { createHash } from "crypto"
+import write_dot from "./subroutines/write-dot"
 
 interface StatusArgs {
   project_id?: ProjectId
@@ -39,6 +41,63 @@ class NGMApi {
         ngm_dot.modules.filter(m => project.modules_ids.includes(m.id))
     }
     return status(ngm_dot.modules)
+  }
+
+  public async project_create(name: string, branch: string) {
+
+    if (this.ngm_dot.projects.find(p => p.name === name)) throw new Error('That project already exists')
+
+    const partial_project: SomePartial<Project, 'id'> = {
+      name,
+      branch,
+      modules_ids: []
+    }
+    const project: Project = {
+      id: createHash('md5').update(JSON.stringify(partial_project)).digest('hex'),
+      ...partial_project
+    }
+
+    const ngm_dot = {...this.ngm_dot}
+
+    ngm_dot.project_map[project.id] = project
+    ngm_dot.projects.push(project)
+
+    if (!await write_dot(ngm_dot)) throw new Error('Failed to create project. Could not write dot file')
+
+  }
+
+  public async project_add(proj_id: ProjectId, ...module_ids: ModuleId[]) {
+
+    if (!this.ngm_dot.project_map[proj_id]) throw new Error('No project with that id')
+    if (module_ids.some(mid => !Boolean(this.ngm_dot.module_map[mid]))) throw new Error('One or more invalid module ids')
+    
+    const ngm_dot = {...this.ngm_dot}
+    const project = ngm_dot.project_map[proj_id]
+
+    project.modules_ids = [...project.modules_ids, ...module_ids].filter((v,i,a) => a.indexOf(v) === i)
+
+    ngm_dot.project_map[proj_id] = project
+    ngm_dot.projects.splice(ngm_dot.projects.findIndex(p => p.id === proj_id), 1, project)
+
+    if (!await write_dot(ngm_dot)) throw new Error('Failed to add modules. Could not write dot file')
+
+  }
+
+  public async project_remove(proj_id: string, ...module_ids: string[]) {
+    
+    if (!this.ngm_dot.project_map[proj_id]) throw new Error('No project with that id')
+    if (module_ids.some(mid => !Boolean(this.ngm_dot.module_map[mid]))) throw new Error('One or more invalid module ids')
+    
+    const ngm_dot = {...this.ngm_dot}
+    const project = ngm_dot.project_map[proj_id]
+
+    project.modules_ids = project.modules_ids.filter(mid => !module_ids.includes(mid))
+
+    ngm_dot.project_map[proj_id] = project
+    ngm_dot.projects.splice(ngm_dot.projects.findIndex(p => p.id === proj_id), 1, project)
+
+    if (!await write_dot(ngm_dot)) throw new Error('Failed to remove modules. Could not write dot file')
+
   }
 
 }
--- src/cli-commands/project.ts
+++ src/cli-commands/project.ts
@@ -3,9 +3,11 @@ import { CommandFn } from ".";
 import { SequenceFunc } from "../utils/exec-sequence";
 import { CLIContext } from "../cli";
 import { Module } from "../interfaces/ngm-dot";
+import displayProcess from '../utils/display-process';
+import chalk from 'chalk';
 
 interface ProjectBuffer {
-  sub_cmd: string
+  sub_cmd: 'create' | 'add' | 'remove' | 'list' | 'detail'
   args: string[]
 }
 
@@ -18,7 +20,9 @@ export const args_parser: SequenceFunc<CLIContext> = (context, args, _next, err)
 
   const buff: SomePartial<ProjectBuffer, 'sub_cmd'> = { args: [] }
   args.forEach((arg): any => {
-    if (!buff.sub_cmd && ['create', 'add', 'remove'].includes(arg)) return buff.sub_cmd = arg
+    if (!buff.sub_cmd && ['create', 'add', 'remove', 'list', 'detail'].includes(arg))
+      return buff.sub_cmd = arg as ProjectBuffer['sub_cmd']
+
     if (!arg.startsWith('-')) buff.args.push(arg)
   })
 
@@ -26,23 +30,80 @@ export const args_parser: SequenceFunc<CLIContext> = (context, args, _next, err)
   switch(buff.sub_cmd) {
     case 'create':
       if (context.project_id) return err('That project already existed')
-      if (buff.args.length < 2) return err('project create requires <project-name> and <new-branch-name>')
+      if (buff.args.length < 2) return err(chalk`project create requires {yellow <project-name>} and {yellow <new-branch-name>}`)
       if (buff.args.length > 2) return err('To many arguments')
       break
     case 'add':
     case 'remove':
-      if (!context.project_id) return err(buff.args.length > 0 ? `Unknown project: ${buff.args[0]}` : 'Missing project name')
-      if (buff.args.length < 2) return err(`project ${buff.sub_cmd} requires <project-name> and <...repo-path>`)
-      const non_module_paths = get_non_repo_paths(context.ngm_dot.modules, buff.args.slice(1))
-      if (non_module_paths.length > 0) return err(`Directories ${non_module_paths.map(p => relative(process.cwd(), p)).join(', ')} are not repositories`)
+      if (!context.project_id) return err(buff.args.length > 0 ? `Unknown project: ${chalk.yellow(buff.args[0])}` : 'Missing project name')
+      if (buff.args.length === 0) return err(chalk`project ${buff.sub_cmd} requires {yellow <project-name>} and {yellow <...repo-path>}`)
+      const non_module_paths = get_non_repo_paths(context.ngm_dot.modules, buff.args)
+      if (non_module_paths.length > 0) return err(`Director${
+          non_module_paths.length > 1
+            ? 'ies'
+            : 'y'
+        } ${non_module_paths.map(p => chalk.yellow(relative(process.cwd(), p) || './')).join(', ')} ${
+          non_module_paths.length > 1
+            ? 'are not repositories'
+            : 'is not a repository'
+        }`)
+      buff.args = buff.args
+        .map(a => (context.ngm_dot.modules.find(m => m.path === resolve(process.cwd(), a)) as Module).id)
       break
+    case 'detail':
+      if (!context.project_id) return err(buff.args.length > 0 ? `Unknown project: ${chalk.yellow(buff.args[0])}` : 'Missing project name')
   }
 
   args.splice(args.indexOf(buff.sub_cmd))
   buff.args.forEach(arg => args.splice(args.indexOf(arg)))
 
+
   context.command_buffer = buff
 
 }
-const project_command: CommandFn = async (_api, context) => console.log('project command', context)
+const project_command: CommandFn = async (api, context) => {
+
+  const command_buffer: ProjectBuffer = context.command_buffer
+  switch (command_buffer.sub_cmd) {
+    case 'create':
+      displayProcess<void>('Creating project', api.project_create(command_buffer.args[0], command_buffer.args[1]))
+      break
+
+    case 'add':
+      displayProcess<void>('Adding repositories', api.project_add(context.project_id || '', ...command_buffer.args))
+      break
+
+    case 'remove':
+      displayProcess<void>('Removing repositories', api.project_remove(context.project_id || '', ...command_buffer.args))
+      break
+
+    case 'list':
+      console.log(
+        [
+          'ID                              \tName\tBranch',
+          ...context.ngm_dot.projects
+            .map(p => chalk`{gray ${p.id}}\t${p.name}\t${p.branch}`)
+        ].join('\n')
+      )
+      break
+
+    case 'detail':
+      const project = context.ngm_dot.project_map[context.project_id || '']
+      const modules = project.modules_ids.map(mid => context.ngm_dot.module_map[mid])
+      console.log(
+        [
+          `ID: ${project.id}`,
+          `Name: ${project.name}`,
+          `Branch: ${project.branch}`,
+          `Modules: ${modules.length === 0 ? chalk.gray('none') : ''}`,
+          ...modules.map(m => chalk`  {gray ${m.id}}\t${relative(process.cwd(), m.path) || './'}\t${m.url}`)
+        ].join('\n')
+      )
+      break
+
+    default:
+      console.log(context)
+  }
+
+}
 export default project_command
