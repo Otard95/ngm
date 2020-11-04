eb6c66bd04bc0f1255a63e35cfa3a57f24508a67
StianM <stian.myklebostad@schibsted.com>
2020-11-04 10:01:23 +0100

    Add equality checker function for intersect

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,3 +1,10 @@
-export const intersect = <T>(arr1: T[], arr2: T[]): T[] => {
+import { partial } from "lodash"
+
+const intersect_default_eqfn = (a: any,b: any)=>a===b
+intersect_default_eqfn.IS_DEFAULT = true
+export const intersect = <T>(arr1: T[], arr2: T[], eqfn: (a: T, b: T) => boolean = intersect_default_eqfn): T[] => {
+  if ((eqfn as typeof intersect_default_eqfn).IS_DEFAULT) {
+    return arr1.filter(v => arr2.findIndex(partial(eqfn, v)) > -1)
+  }
   return arr1.filter(v => arr2.includes(v))
 }
