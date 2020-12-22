1e461700b7080a58fe3fa43a681adfe1bd7d2d31
StianM <stian.myklebostad@schibsted.com>
2020-12-22 13:42:41 +0100

    Expand displayProcess() to be able to display multiple at a time

--- src/utils/display-process.ts
+++ src/utils/display-process.ts
@@ -1,8 +1,13 @@
+import rl from 'readline'
+import { promisify } from 'util'
 import chalk from "chalk";
+import caretPos from "./caretPos";
+import { pad_right_to } from "./pad-str";
+import { arrayForEachSequential, makeTrackablePromise, TrackablePromise } from "./promise";
 
-const displayProcess = <R>(message: string, promise: Promise<R>): Promise<R> => {
+const loading_icons = ['⠏', '⠛', '⠹', '⠼', '⠶', '⠧']
+const displayProcessSingle = <R>(message: string, promise: Promise<R>): Promise<R> => {
 
-  const loading_icons = ['⠏', '⠛', '⠹', '⠼', '⠶', '⠧']
   let i = 0
   process.stdout.write(`${message} ${chalk.blue(loading_icons[i])}`)
   const loading_interval = setInterval(() => {
@@ -10,7 +15,7 @@ const displayProcess = <R>(message: string, promise: Promise<R>): Promise<R> => 
       i = (i + 1) % loading_icons.length
       process.stdout.write(chalk.blue(`${loading_icons[i]}`))
     })
-  }, 200)
+  }, 100)
   return promise
     .then(data => new Promise<R>((res) => {
       clearInterval(loading_interval)
@@ -26,6 +31,75 @@ const displayProcess = <R>(message: string, promise: Promise<R>): Promise<R> => 
         rej(err)
       })
     }))
+
+}
+
+interface Process<R> {
+  label: string;
+  promise: TrackablePromise<R> 
+}
+export interface ProcessInput<R> {
+  label: string
+  promise: Promise<R> 
+}
+export interface DisplayProcess {
+  <R>(message: string, promise: Promise<R>): Promise<R>
+  <R>(...precesses: ProcessInput<R>[]): Promise<R[]>
+}
+const displayProcess: DisplayProcess = async <R>(...args: unknown[]) => {
+
+  if (args.length === 2 && typeof args[0] === 'string')
+    return await displayProcessSingle<R>(args[0], args[1] as Promise<R>)
+
+  const processes: Process<R>[] = (args as ProcessInput<R>[])
+    .map(({ label, promise }) => ({ label, promise: makeTrackablePromise(promise) }))
+
+  const loading_col = processes
+    .reduce((col, { label }) => Math.max(col, label.length + 1), 0)
+
+  const startPos = process.env['NODE_ENV'] === 'debug' ? {row: 0, col:0} : await caretPos()
+  const out = process.stdout
+
+  // initial status print
+  processes
+    .forEach((p) => out.write(
+      `${pad_right_to(p.label, loading_col)}${chalk.blue(loading_icons[0])}\n`
+    ))
+
+  // setup status update function
+  let loading_i = 0
+  const printStatus = async () => {
+    loading_i = (loading_i + 1) % loading_icons.length
+    await arrayForEachSequential(processes, async (p, i) => {
+
+      await promisify(
+        (rl.cursorTo)
+      )(out, loading_col, startPos.row + i)
+
+      const status = p.promise.resolved
+        ? chalk.green('[DONE]')
+        : p.promise.rejected
+          ? chalk.red('[ERROR]')
+          : chalk.blue(loading_icons[loading_i])
+
+      await promisify(out.write.bind(out))(status)
+
+    })
+    console.log()
+  }
+  // start status update interval
+  const loading_interval = setInterval(printStatus, 100)
+
+  // await result
+  const res = await Promise.all(processes.map(p => p.promise))
+  // stop status update cycle
+  clearInterval(loading_interval)
+  // clear update output
+  await promisify(rl.cursorTo)(out, startPos.col, startPos.row)
+  await promisify(rl.clearScreenDown)(out)
+  await promisify(rl.cursorTo)(out, startPos.col, startPos.row)
+  
+  return res
 
 }
 export default displayProcess
