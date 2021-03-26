281e97e22152951745116b951df736028b11f871
StianM <stian.myklebostad@schibsted.com>
2021-03-26 13:24:30 +0100

    Formatting and better error messages

--- src/cli-commands/project.ts
+++ src/cli-commands/project.ts
@@ -23,20 +23,30 @@ export const args_parser: SequenceFunc<CLIContext> = (context, args, _next, err)
   switch(buff.sub_cmd) {
     case 'create':
       if (context.project_id) return err('That project already existed')
-      if (buff.args.length < 2) return err(chalk`project create requires {yellow <project-name>} and {yellow <new-branch-name>}`)
+      if (buff.args.length < 2)
+        return err(chalk`project create requires {yellow <project-name>} and {yellow <new-branch-name>}`)
       if (buff.args.length > 2) return err('To many arguments')
       break
     case 'add':
     case 'remove':
       if (!context.project_id)
         return err(buff.args.length > 0 ? `Unknown project: ${chalk.yellow(buff.args[0])}` : 'Missing project name')
-      if (!context.repository_ids || context.repository_ids.length === 0)
-        return err(chalk`project ${buff.sub_cmd} requires {yellow <project-name>} and {yellow <...repo-path>}`)
+      if (!context.repository_ids || context.repository_ids.length === 0) {
+        const unknown_repos = args.length > 0
+          ? `\n\t${args.join(', ')} - ${
+              args.length > 1 ? 'are' : 'is'
+            } not ${
+              args.length > 1 ? 'repositories' : 'a repository'
+            }`
+          : ''
+        return err(chalk`project ${buff.sub_cmd} requires {yellow <project-name>} and {yellow <...repo-path>}${unknown_repos}`)
+      }
 
       buff.args = context.repository_ids
       break
     case 'detail':
-      if (!context.project_id) return err(buff.args.length > 0 ? `Unknown project: ${chalk.yellow(buff.args[0])}` : 'Missing project name')
+      if (!context.project_id)
+        return err(buff.args.length > 0 ? `Unknown project: ${chalk.yellow(buff.args[0])}` : 'Missing project name')
   }
 
   buff.args.forEach(arg => args.splice(args.indexOf(arg)))
@@ -61,8 +71,16 @@ const project_command: CommandFn = async (api, context) => {
       const pid = context.project_id
       if (!isString(pid)) throw new Error('Missing project')
 
-      await displayProcess<void>('Adding repositories', api.project_add(pid, ...command_buffer.args))
-      await displayProcess('Checking out repositories', Promise.all(command_buffer.args.map(rid => api.checkout(rid, context.ngm_dot.project_map[pid].branch))))
+      await displayProcess<void>(
+        'Adding repositories',
+        api.project_add(pid, ...command_buffer.args)
+      )
+      await displayProcess(
+        'Checking out repositories',
+        Promise.all(command_buffer.args.map(rid =>
+          api.checkout(rid, context.ngm_dot.project_map[pid].branch)
+        ))
+      )
 
       await api.save_dot()
       }
@@ -72,7 +90,10 @@ const project_command: CommandFn = async (api, context) => {
       const pid = context.project_id
       if (!isString(pid)) throw new Error('Missing project')
 
-      await displayProcess<void>('Removing repositories', api.project_remove(context.project_id || '', ...command_buffer.args))
+      await displayProcess<void>(
+        'Removing repositories',
+        api.project_remove(context.project_id || '', ...command_buffer.args)
+      )
       await displayProcess(
         'Checking out repositories',
         Promise.all(command_buffer.args.map(rid => {
@@ -88,8 +109,10 @@ const project_command: CommandFn = async (api, context) => {
       break
 
     case 'list':
-      const name_len = context.ngm_dot.projects.reduce((acc, p) => Math.max(acc, p.name.length), 0)
-      const branch_len = context.ngm_dot.projects.reduce((acc, p) => Math.max(acc, p.branch.length), 0)
+      const name_len = context.ngm_dot.projects
+        .reduce((acc, p) => Math.max(acc, p.name.length), 0)
+      const branch_len = context.ngm_dot.projects
+        .reduce((acc, p) => Math.max(acc, p.branch.length), 0)
 
       console.log(
         [
@@ -102,10 +125,13 @@ const project_command: CommandFn = async (api, context) => {
 
     case 'detail':
       const project = context.ngm_dot.project_map[context.project_id || '']
-      const repositories = project.repository_ids.map(mid => context.ngm_dot.repository_map[mid])
-      const path_len = repositories.reduce((acc, m) => Math.max(acc, (relative(process.cwd(), m.path) || './').length), 0)
-      const url_len =  repositories.reduce((acc, m) => Math.max(acc, m.url.length), 0)
-      
+      const repositories = project.repository_ids
+        .map(mid => context.ngm_dot.repository_map[mid])
+      const path_len = repositories
+        .reduce((acc, m) => Math.max(acc, (relative(process.cwd(), m.path) || './').length), 0)
+      const url_len =  repositories
+        .reduce((acc, m) => Math.max(acc, m.url.length), 0)
+        
       console.log(
         [
           `ID: ${project.id}`,
