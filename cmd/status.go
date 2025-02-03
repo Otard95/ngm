5cee7b28585e54e0059e1b87cf1720bc4707431a
Stian Myklebostad <stian.myklebostad@schibsted.com>
2025-02-03 10:28:21 +0100

    feat(status): Setup status cmd

--- cmd/status.go
+++ cmd/status.go
@@ -22,23 +22,21 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/Otard95/ngm/git"
+	"github.com/Otard95/ngm/log"
 	"github.com/spf13/cobra"
 )
 
 // statusCmd represents the status command
 var statusCmd = &cobra.Command{
 	Use:   "status",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Check the status of repositories",
+	Long: `Recursively checks the status of this and each child repository under the
+current directory.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("status called")
+		log.Debugln("Running status cmd")
+		git.Status()
+		log.Debugln("Finished status cmd")
 	},
 }
 
