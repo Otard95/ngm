550cd26a09ad70708dd4b5f47938243a6329cf7f
Stian Myklebostad <stian.myklebostad@schibsted.com>
2025-02-03 10:27:16 +0100

    feat(ui): Add function to display a number of parallel tasks

---
+++ ui/styles.go
@@ -0,0 +1,8 @@
+package ui
+
+import "github.com/charmbracelet/lipgloss"
+
+var (
+	SuccessStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#40a02b"))
+	ErrorStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("#d20f39"))
+)
